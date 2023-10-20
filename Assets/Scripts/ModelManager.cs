6cd25cd4eee7803bbd29c1cc783391e42bf4ab9c
DaniEsparza1712 <0244218@up.edu.mx>
2023-10-20 12:03:03 -0600

    AR Chemistry Lab Main Project

---
+++ Assets/Scripts/ModelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelManager : MonoBehaviour {
+
+	public GameObject[] Models;
+    void Start()
+    {
+            //Disable All models at start
+            DisableAll();
+    }
+
+    void DisableAll()
+    {
+        foreach (var item in Models)
+        {
+            item.SetActive(false);
+        }
+    }
+
+    void SetModel(int index)
+    {
+        //Disable All other models before setting
+        DisableAll();
+        Models[index].SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public GameObject ModelsManager;
+    public Sprite[] selectedSprites;
+    public Sprite[] NormalSprites;
+    public Image[] SliderImages;
+
+	// Use this for initialization
+	void Start () {
+        DeselectAll();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SelectModel(int index)
+    {
+        ModelsManager.SendMessage("SetModel", index);
+        //Set all Images to be de selected
+        DeselectAll();
+        SliderImages[index].sprite = selectedSprites[index];
+    }
+
+    void DeselectAll()
+    {
+        for (int i = 0; i < SliderImages.Length; i++)
+        {
+            SliderImages[i].sprite = NormalSprites[i];
+        }
+    }
+
+}
